5f502e6f8e8df101a644a55fa0076e6ffeca2aa8
André Noack <noackorama@googlemail.com>
2023-02-13 15:11:17 +0100

    Sperrregel für kopieren beachten

--- views/action/checkbox.php
+++ views/action/checkbox.php
@@ -1,3 +1,3 @@
-<? if (Seminar_Perm::get()->have_studip_perm('dozent', $course_id)) : ?>
+<? if (Seminar_Perm::get()->have_studip_perm('dozent', $course_id) && !LockRules::check($course_id, 'seminar_copy')) : ?>
 <input type="checkbox" name="c[]" value="<?= htmlReady($course_id) ?>">
 <? endif ?>
